3252fa4051bcfc3d27807dcbded182b10b4397f8
Timi <tunmisebabade@gmail.com>
2025-05-30 19:03:55 +0100

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,8 +4,8 @@ import { TrainingHub } from "~/components/trainingHub";
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Home Page" },
+    { name: "description", content: "Welcome!" },
   ];
 }
 
